87b2b31c61d3163f684d0962e9c7bff393421f01
dengpan <571143755@qq.com>
2019-08-10 14:53:02 +0800

    [add]promise用法

---
+++ javascript/ES6/promise.js
@@ -0,0 +1,79 @@
+let promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject('reject')
+  }, 1000)
+})
+promise.then((res) => {
+}, (error) => {
+//   // 传入 onRejected 会先执行 onRejected 函数，再执行 catch
+  console.log('error', error) // error reject
+}).catch(error => {
+  // 在 then 中没有传入 onrejected 函数的时候才会执行 catch 函数
+  console.log('catch', error) // catch reject
+})
+
+function timeout(ms) {
+  return new Promise((resolve, reject) => {
+    console.log('promise 立即执行') // promise 立即执行
+    setTimeout(resolve, ms, 'done')
+  })
+}
+timeout(1000).then(res => {
+  console.log('onfulfilled', res) // onfulfilled done
+})
+
+let getJSON = function (url) {
+  const promise = new Promise((resolve, reject) => {
+    const handler = function () {
+      if (this.readyState !== 4) {
+        return
+      }
+      if (this.status === 200) {
+        resolve(this.response)
+      } else {
+        reject(new Error(this.statusText))
+      }
+    }
+    let client = new XMLHttpRequest()
+    client.open('GET', url)
+    client.onreadystatechange = handler
+    client.setRequestHeader('Accept', 'application/json')
+    client.responseType = 'json'
+    client.send()
+  })
+  return promise
+}
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve('p1 resolve'), 1000) //
+// })
+// const p2 = new Promise((resolve, reject) => {
+//   // 传递的为 promise, 最后结果有传递的 promise 的状态决定
+//   // 传递到 reject 函数中，则直接将当前状态作为值传入，并进入 onrejected 的回调
+//   // 传递到 resolve 函数中，则会等待传入的状态，并由传入的状态决定进入哪一个状态的回调
+//   setTimeout(() => reject(p1))
+// })
+// p2.then((res) => {
+//   console.log('res', res)
+// }, err => {
+//   console.log('err', err)
+// }).catch(error => {
+//   console.log('catch ', error)
+// })
+const p1 = new Promise((resolve, reject) => {
+  // resolve('resolve')
+  // reject('reject')
+  throw new Error('这是一个错误')
+  // console.log('继续执行') // resolve, reject 不会终结 Promise 的参数函数的执行
+})
+p1.then(res => {
+  console.log('p1 res', res)
+}
+// , error => {
+//   // 错误总是先被 then 中的 onrejected 函数捕获
+//   console.log('p1 error', error) // p1 error reject: 调用reject('reject')时执行的
+// }
+).then(res => {
+  console.log('res', res)
+}).catch(error => {
+  console.log('p1 catch', error)
+})
