fb2439cfb350fe3b35d870616958c33f9ca84c17
dengpan <571143755@qq.com>
2019-11-14 11:24:28 +0800

    [add]单链表反转

--- dataStructure/linkList.js
+++ dataStructure/linkList.js
@@ -4,21 +4,41 @@ function SingleLinkNode(val, next) {
   this.value = val
   this.next = next || null
 }
-SingleLinkNode.prototype.setNext = function (singleLinkNode) {
-  this.next = singleLinkNode
-}
-let head = new SingleLinkNode() // 头部节点
-let POINTER = null  // 游标指针
-for (let i = 0; i < 5; i++) {
-  let singleNode = new SingleLinkNode(i)
-  if (i === 0) {
-    head.setNext(singleNode)
-  } else {
-    if (POINTER) {
-      POINTER.setNext(singleNode)
+// 生成单向链表
+function generateSingleLinkList () {
+  let head = new SingleLinkNode() // 头部节点
+  let POINTER = null  // 游标指针
+  for (let i = 0; i < 5; i++) {
+    let singleNode = new SingleLinkNode(i)
+    if (i === 0) {
+      head.next = singleNode
+    } else {
+      if (POINTER) {
+        POINTER.next = singleNode
+      }
     }
+    POINTER = singleNode
   }
-  POINTER = singleNode
+  return {
+    head: head
+  }
 }
-// {"next":{"value":0,"next":{"value":1,"next":{"value":2,"next":{"value":3,"next":{"value":4,"next":null}}}}}}
-console.log('singleLinkNode', JSON.stringify(head))
+// {"head":{"next":{"value":0,"next":{"value":1,"next":{"value":2,"next":{"value":3,"next":{"value":4,"next":null}}}}}}}
+console.log('singleLinkNode', JSON.stringify(generateSingleLinkList()))
+
+// 单向列表反转
+function singleLinkListReverse () {
+  let pre = null
+  let next = null
+  let singleLinkReverse = generateSingleLinkList()
+  let POINTER = singleLinkReverse.head
+  while (POINTER) {
+    next = POINTER.next
+    POINTER.next = pre
+    pre = POINTER
+    POINTER = next
+  }
+  return pre
+}
+var reverseSingleLinkList = singleLinkListReverse()
+console.log('singleLinkListReverse', JSON.stringify(reverseSingleLinkList))
