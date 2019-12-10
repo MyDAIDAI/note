823f835d05da4f27499dea073dea4fe3e7e6dcf8
dengpan <571143755@qq.com>
2019-12-10 23:19:49 +0800

    [add]递归求和

--- dataStructure/recursion.js
+++ dataStructure/recursion.js
@@ -21,3 +21,31 @@ function flatArr(arr) {
   return result
 }
 console.log('flatArr', flatArr(arr))
+
+// 递归爆栈
+// 执行数值 100, 进程卡死
+// function sum(n) {
+//   if (n <= 2) {
+//     return 1
+//   } else {
+//     return sum(n - 1) + sum(n - 2)
+//   }
+// }
+// console.time('sum')
+// console.log(sum(100))
+// console.timeEnd('sum')
+
+// 进行尾递归优化后的代码
+// 执行时间: 0.730ms
+function optimizeSum(n, a = 1, b = 1) {
+  if (n <= 2) {
+    return b
+  } else {
+    return optimizeSum(n - 1, b, a + b)
+  }
+}
+console.time('optimizeSum')
+console.log(optimizeSum(100))
+console.timeEnd('optimizeSum')
+
+// TODO: what is 尾递归优化？
